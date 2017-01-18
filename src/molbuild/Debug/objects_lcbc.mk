c59c0a033cf6f9cc29ad9565736e34c6c82262ff
Exarctus <nicholas.browning@epfl.ch>
2017-01-18 13:27:55 +0100

    Second commit

---
+++ src/molbuild/Debug/objects_lcbc.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopenbabel -lboost_filesystem -lboost_system
+
